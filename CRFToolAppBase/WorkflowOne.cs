e28a928b6008df9e615ad9ceb04634c302d30794
MarlonWelter <mrmwelter@gmail.com>
2017-10-10 15:05:21 +0200

    add some training logic to workflow one

--- CRFToolAppBase/WorkflowOne.cs
+++ CRFToolAppBase/WorkflowOne.cs
@@ -1,4 +1,6 @@
 ﻿using CodeBase;
+using CRFBase;
+using CRFBase.OLM;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -18,23 +20,60 @@ namespace CRFToolAppBase
             //	-Graphstructure
             //    - Training Data
             //    - 2 Node Classifications
+            var testGraphs = new List<GWGraph<CRFNodeData, CRFEdgeData, CRFGraphData>>();
 
             //    -n characteristics for each node
-            var request = new UserInput();
-            request.TextForUser = "Please select the folder with your graph training data.";
-            request.Request();
-            GraphDataFolder = request.UserText;
-
-            foreach (var file in Directory.EnumerateFiles(GraphDataFolder))
             {
-                var graph = JSONX.LoadFromJSON<GWGraph<CRFNodeData, CRFEdgeData, CRFGraphData>>(file);
-            }
+                var request = new UserInput();
+                request.TextForUser = "Please select the folder with your graph training data.";
+                request.Request();
+                GraphDataFolder = request.UserText;
 
+                foreach (var file in Directory.EnumerateFiles(GraphDataFolder))
+                {
+                    var graph = JSONX.LoadFromJSON<GWGraph<CRFNodeData, CRFEdgeData, CRFGraphData>>(file);
+                    testGraphs.Add(graph);
+                }
+            }
 
             //-Step 1:
 
             //   -discretize characteristics
-            //   - Use Training Pede
+            #region Use Training Pede
+            {
+                var isingConformityParameter = 0.5;
+                var isingCorrelationParameter = 0.5;
+                var isingModel = new IsingModel(isingConformityParameter, isingCorrelationParameter);
+                var request = new OLMRequest(OLMVariant.Ising, testGraphs);
+                request.BasisMerkmale = new BasisMerkmal<ICRFNodeData, ICRFEdgeData, ICRFGraphData>[]
+                    { new IsingMerkmalNode(), new IsingMerkmalEdge() };
+                //TODO: loss function auslagern
+                request.LossFunctionValidation = (a, b) =>
+                {
+                    var loss = 0.0;
+                    for (int i = 0; i < a.Length; i++)
+                    {
+                        loss += a[i] != b[i] ? 1 : 0;
+                    }
+                    return loss / a.Length;
+                };
+
+                request.Request();
+
+                var olmResult = request.Result;
+
+
+                // update Ising parameters in IsingModel
+                isingModel.ConformityParameter = olmResult.ResultingWeights[0];
+                isingModel.CorrelationParameter = olmResult.ResultingWeights[1];
+
+                // zugehörige Scores erzeugen für jeden Graphen (auch Evaluation)
+                //foreach (var graph in graphList)
+                //{
+                //    isingModel.CreateCRFScore(graph);
+                //}
+            }
+            #endregion
 
             //- Step2:
 
