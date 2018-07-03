015a5dddbd436cc0103bbe80123e92d4a1c96f1d
MarlonWelter <mrmwelter@gmail.com>
2018-07-03 15:04:40 +0200

    prepare sofrwaregraph usecase 1

---
+++ CRFToolAppBase/Software Graph/SoftwareGraphAnalysis_1.cs
@@ -0,0 +1,32 @@
+﻿using CodeBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRFToolAppBase.Software_Graph
+{
+    /*
+     *  model: Ising
+     *  pre trained data for conformity and correlation
+     * 
+     * */
+    public class SoftwareGraphAnalysis_1 : GWRequest<SoftwareGraphAnalysis_1>
+    {
+        public SoftwareGraphAnalysis_1(string graphInputFolder, string outputFolder, double conformityParameter, double correlationParameter, int viterbiHeuristicMemoryParameter)
+        {
+            GraphInputFolder = graphInputFolder;
+            OutputFolder = outputFolder;
+            CorrelationParameter = correlationParameter;
+            ConformityParameter = conformityParameter;
+            ViterbiHeuristicMemoryParameter = viterbiHeuristicMemoryParameter;
+        }
+        public double ConformityParameter { get; set; }
+        public double CorrelationParameter { get; set; }
+        public string GraphInputFolder { get; set; }
+        public string OutputFolder { get; set; }
+        public int ViterbiHeuristicMemoryParameter { get; set; }
+
+    }
+}
---
+++ CRFToolAppBase/Software Graph/SoftwareGraphAnalysis_1_Manager.cs
@@ -0,0 +1,50 @@
+﻿using CodeBase;
+using CRFBase;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRFToolAppBase.Software_Graph
+{
+    class SoftwareGraphAnalysis_1_Manager : GWManager<SoftwareGraphAnalysis_1>
+    {
+        public SoftwareGraphAnalysis_1_Manager()
+        {
+
+        }
+        protected override void OnRequest(SoftwareGraphAnalysis_1 request)
+        {
+            // load data
+            var graphs = new List<GWGraph<ICRFNodeData, ICRFEdgeData, ICRFGraphData>>();
+            foreach (var file in Directory.GetFiles(request.GraphInputFolder))
+            {
+                using (var reader = new StreamReader(file))
+                {
+                    // parse graph
+                }
+            }
+
+            // compute viterbi
+            var results = new List<SolveInference>();
+            var isingModel = new IsingModel(request.ConformityParameter, request.CorrelationParameter);
+            foreach (var graph in graphs)
+            {
+                isingModel.CreateCRFScore(graph);
+                var viterbiRequest = new SolveInference(graph, 2, request.ViterbiHeuristicMemoryParameter);
+                viterbiRequest.Request();
+                results.Add(viterbiRequest);
+            }
+
+            // store results
+            Directory.CreateDirectory(request.OutputFolder);
+            foreach (var result in results)
+            {
+                result.SaveAsJSON(result.Graph.Name + "-result.txt");
+            }
+
+        }
+    }
+}
